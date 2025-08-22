59ae1b5dd723b8f44c333ab248bb6114f6b0b47c
Dave Tang <davetingpongtang@gmail.com>
2025-08-22 15:59:51 +0900

    Create and use a specific user

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -2,14 +2,15 @@
 
 set -euo pipefail
 
-image=nvim
-ver=24.04
+NAME=nvim
+VER=24.04
+SCRIPTDIR=$(dirname $0)
 
-docker build -t davetang/${image}:${ver} .
+cd ${SCRIPTDIR} && docker build -t davetang/${NAME}:${VER} .
 
 >&2 echo Build complete
 >&2 echo -e "Run the following to push to Docker Hub:\n"
 >&2 echo docker login
->&2 echo docker push davetang/${image}:${ver}
+>&2 echo docker push davetang/${NAME}:${VER}
 
 exit 0
