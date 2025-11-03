bf84e354ed8b2b034f33609c1538f3e927ce9eb1
Dave Tang <davetingpongtang@gmail.com>
2025-11-03 17:19:26 +0900

    Custom command to open practice.md

--- init.lua
+++ init.lua
@@ -202,3 +202,12 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find f
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- custom :Practice command
+vim.api.nvim_create_user_command(
+  'Practice',
+  function()
+    vim.cmd('vsplit ~/.config/nvim/practice.md')
+  end,
+  { desc = 'Things to practise' }
+)
