c66752adb6b414fea71bd015949f9ad8a96c5cf9
Dave Tang <davetingpongtang@gmail.com>
2025-12-23 15:25:21 +0900

    rnoweb required by R-nvim/R.nvim

--- init.lua
+++ init.lua
@@ -155,7 +155,7 @@ vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ async = true })
 -- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#modules
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "r", "python", "bash", "groovy", "make", "perl", "sql", "yaml", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+  ensure_installed = { "r", "rnoweb", "python", "bash", "groovy", "make", "perl", "sql", "yaml", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
